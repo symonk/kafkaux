3d147394d3d0c4b729c729ad9dc4e34bd27aa1bf
symonk <jackofspaces@gmail.com>
2025-09-20 16:26:07 +0100

    add `pre-commit`, lint files

--- src/kafkaux/main.py
+++ src/kafkaux/main.py
@@ -1,24 +1,24 @@
 import typer
 
-app = typer.Typer() # Todo: Update args
+app = typer.Typer()  # Todo: Update args
 
 
 @app.command()
-def produce():
-    ...
+def produce(): ...
+
 
 @app.command()
-def consume():
-    ...
+def consume(): ...
+
 
 @app.command()
-def meta():
-    ...
+def meta(): ...
 
 
 def main() -> int:
     app()
     return 0
-    
+
+
 if __name__ == "__main__":
     raise SystemExit(main())
