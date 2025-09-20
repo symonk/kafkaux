80f266e5208138b0022cfd79139680fd527a9efa
symonk <jackofspaces@gmail.com>
2025-09-20 16:20:51 +0100

    basic github actions workflows; add dependencies, set python 3.11+

---
+++ src/kafkaux/main.py
@@ -0,0 +1,24 @@
+import typer
+
+app = typer.Typer() # Todo: Update args
+
+
+@app.command()
+def produce():
+    ...
+
+@app.command()
+def consume():
+    ...
+
+@app.command()
+def meta():
+    ...
+
+
+def main() -> int:
+    app()
+    return 0
+    
+if __name__ == "__main__":
+    raise SystemExit(main())
